4f0c56b8ab617ba2b8e46e8ffb3839bcf1d154b3
Sigifredo Escobar Gómez <sigifredo89@gmail.com>
2012-02-06 19:13:09 -0500

    Creamos el formulario básico para guardar proyectos.

--- prototipos/application/controllers/RegProyController.php
+++ prototipos/application/controllers/RegProyController.php
@@ -17,7 +17,7 @@ class RegProyController extends Zend_Controller_Action
     {
         $form=new Zend_Form;
         $form->setAttrib('class','regproy');
-        $form->setAction($this->view->url(array("controller" => "reg-proy", "action" => "guardar-datos-basicos")))
+        $form->setAction($this->view->url(array("controller" => "reg-proy", "action" => "ingresar-datos-basicos")))
         ->setMethod('post');
 
         $form->addElement('text','nombre',array('label'=>'Nombre','required'=>true,'filter'=>'StringToLower','validator'=>'alfa','validator'=>'StringLength',false,array(4,25)));
@@ -31,10 +31,50 @@ class RegProyController extends Zend_Controller_Action
 
     public function ingresarDatosBasicosAction()
     {
-        // action body
+        if(!$this->getRequest()->isPost())
+        {
+            echo "<h4 id='infnews'>Datos de noticia</h4>";
+            echo $form;
+            return;
+        }
+
+        $form=new Zend_Form;
+        $form->setAttrib('class','ingresar');
+        $form->setAction($this->view->url(array("controller" => "reg-proy", "action" => "guardar-proyecto")))
+             ->setMethod('post');
+
+        $values = $this->getRequest()->getParams();
+        $form->addElement('hidden','proyecto',array('label'=>'Proyecto: '.$values['nombre']));
+        $form->addElement('hidden','descripcion',array('label'=>'Descripción: '.$values['descripcion']));
+
+        // Integrante(s)
+        $form->addElement('hidden','integrantes',array('label'=>'Integrantes'));
+        $this->ingresarIntegrante($form);
+
+        // Comprimoso(s)
+        $form->addElement('hidden','compromisos',array('label'=>'Compromisos'));
+        $this->ingresarCompromiso($form);
+
+        $form->addElement('submit','guardar',array('label'=>'Guardar'));
+
+        echo $form;
+        
     }
 
+    public function ingresarIntegrante($form)
+    {
+        $form->addElement('text','nombre-integrante',array('label'=>'Nombre','required'=>true,'filter'=>'StringToLower','validator'=>'alfa','validator'=>'StringLength',false,array(4,25)));
+
+        $form->addElement('text','documento-integrante',array('label'=>'Documento','required'=>true,'filter'=>'StringToLower','validator'=>'alfa','validator'=>'StringLength',false,array(4,25)));
+
+        $form->addElement('text','correo-integrante',array('label'=>'Correo','required'=>true,'filter'=>'StringToLower','validator'=>'alfa','validator'=>'StringLength',false,array(4,25)));
+    }
+
+    public function ingresarCompromiso($form)
+    {
+        $form->addElement('text','descripcion-compromiso',array('label'=>'Correo del integrante','required'=>true,'filter'=>'StringToLower','validator'=>'alfa','validator'=>'StringLength',false,array(4,25)));
+        $form->addElement('textarea','descripcion-compromiso',array('label'=>'Descripción del compromiso','required'=>true,'filter'=>'StringToLower','validator'=>'alfa','validator'=>'StringLength',false,array(4,25)));
+    }
 
 }
-
 
