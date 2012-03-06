dad55d85ef238a05eb18358a277a535007c614ae
Sigifredo Escobar Gómez <sigifredo89@gmail.com>
2012-03-05 23:10:48 -0500

    Implementamos el ejercicio de la media y desviación estandar.

---
+++ Implentacion/p1/main.cpp
@@ -0,0 +1,55 @@
+
+#include<iostream>
+#include<list>
+#include<cmath>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+typedef std::list<int> lista;
+
+int main()
+{
+    int n;
+    float fMedia = 0.0f;
+    float fDesviacion = 0.0f;
+
+    lista l;
+
+    std::cin >> n;
+
+    // Leemos los n datos.
+    for(int i = 0; i < n; i++)
+    {
+        int iNro;
+
+        std::cin >> iNro;
+        l.push_back(iNro);
+
+        // A medida que leemos calculamos la sumatoria de todos los datos.
+        // para posteriormente calcular la media.
+        fMedia += iNro;
+    }
+
+    // Encontramos la media.
+    fMedia /= n;
+
+    // Recorremos la lista de los números leidos para calcular la sumatoria en
+    // la ecuación de la desviacion estandar
+    for(lista::iterator it = l.begin(); it != l.end(); it++)
+    {
+        float fX = *it;
+
+        // Calculamos la sumatoria.
+        fDesviacion += (fX - fMedia)*(fX - fMedia);
+    }
+
+    // Calculamos la desviación estandar.
+    fDesviacion = std::sqrt(fDesviacion / (n-1.0f));
+
+    // mostramos el resultado en pantalla.
+    cout << "Media:\t\t" << fMedia << endl << "Desviacion:\t" << fDesviacion << endl;
+
+    return 0;
+}
